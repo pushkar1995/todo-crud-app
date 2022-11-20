425ab042e933689b030324e71beddbb56268d636
pushkar gautam <pushkar.gautam1994@gmail.com>
2022-11-20 20:44:05 +0545

    basic folder structure and code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import TodoPage from './Screens/TodoPage';
 
 function App() {
   return (
     <div className="App">
-     
+      <TodoPage />
     </div>
   );
 }
--- src/Components/AddTodoForm.tsx
+++ src/Components/AddTodoForm.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { FC, useState } from 'react'
 
-const AddTodoForm = () => {
-  return (
-    <div>AddTodoForm</div>
+const AddTodoForm: FC = () => {
+    const [task, setTask] = useState<string>('')
+
+    const onNewTaskChangeHandler = (e: any) => {
+        setTask(e.target.value)
+    }
+
+  return ( 
+    <div>
+       <h1>Add Todo Form</h1>
+        <form>
+                <div>
+                    <input
+                        type='text'
+                        className=''
+                        placeholder='add new task'
+                        value={task}
+                        onChange={onNewTaskChangeHandler}
+                    />
+                </div>
+                <div>
+                    <input
+                        type='submit'
+                        className=''
+                        value='Add Task' 
+                    />
+                </div>
+        </form> 
+    </div>
   )
 }
 
