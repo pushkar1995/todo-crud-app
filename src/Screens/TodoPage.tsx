1713eef5fe7e2eff9c75eaee931869cd82f59949
pushkar gautam <pushkar.gautam1994@gmail.com>
2022-11-20 11:55:20 +0545

    basic todo app layout

---
+++ src/Screens/TodoPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AddTodoForm from '../Components/AddTodoForm'
+import TodoList from '../Components/TodoList'
+
+const TodoPage = () => {
+  return (
+    <div>
+        Todo-APP
+        <AddTodoForm />
+        <TodoList />
+    </div>
+  )
+}
+
+export default TodoPage
