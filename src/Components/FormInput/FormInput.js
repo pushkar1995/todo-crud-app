fc54d9bf7032c592b6270d7a460629ec7e3150db
Pushkar Gautam <pushkargautam993@gmail.com>
2023-04-14 16:29:38 +0545

    Basic Register form with JS,CSS and validation

--- src/Components/FormInput/FormInput.js
+++ src/Components/FormInput/FormInput.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './formInput.css'
 
 // type FormInputPropTypes = {
@@ -7,16 +7,29 @@ import './formInput.css'
 
 // const FormInput = (props: FormInputPropTypes) => {
 const FormInput = (props) => {
-    // const { placeholder } = props
+  const [focused, setFocused] = useState(false)
+  const { label, errorMessage, onChange, id, ...inputProps } = props
+
+  const handleFocus = (e) => {
+    setFocused(true)
+  }
   return (
-    <div className='my-4'>
-        {/* <label>Username</label> */}
+    <div className='flex flex-col w-64 my-2'>
+        <label className='py-2 text-grey'>{label}</label>
         <input
           className='p-2 rounded'   
-          placeholder={props.placeholder}
-          ref={props.refer}
+          {...inputProps}
+          onChange={onChange}
+          onBlur={handleFocus}
+          focused={focused.toString()}
+          onFocus={()=> inputProps.name === 'confirmPassword' && setFocused(true)}
+          // ref={props.refer}
           // onChange={(e) => props.setUsername(e.target.value)}
         />
+        <span 
+          className='hidden p-1 text-pinkyred'
+          // className={`${invalid ? 'p-1 text-pinkyred block' : 'hidden'}`}
+        >{errorMessage}</span>
     </div>
   )
 }
--- src/Components/Register/RegisterForm.js
+++ src/Components/Register/RegisterForm.js
@@ -6,27 +6,103 @@ const RegisterForm = () => {
   // const [username, setUsername] = useState('')
   // console.log(username)
   // console.log('re-rendered!!')
-  const usernameRef = useRef()
-  console.log('re-rendered!!')
+
+  // const usernameRef = useRef()
+  // console.log('re-rendered!!')
+
+  const [ values, setValues] = useState({
+    username: '',
+    email: '',
+    birthday: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const inputs = [
+    {
+      id: 1,
+      name: 'username',
+      type: 'text',
+      placeholder: 'Username',
+      errorMessage: 'Username should be 3-16 characters and shouldnt include any special characters',
+      label: 'Username:',
+      pattern: '^[A-Za-z0-9]{3,16}$',
+      required: true
+    },
+    {
+      id: 2,
+      name: 'email',
+      type: 'email',
+      placeholder: 'Email',
+      errorMessage: 'It should be a valid Email Address!',
+      label: 'Email:',
+      required: true
+    },
+    {
+      id: 3,
+      name: 'birthday',
+      type: 'date',
+      placeholder: 'Birthday',
+      errorMessage: '',
+      label: 'Birthday:',
+    },
+    {
+      id: 4,
+      name: 'password',
+      type: 'password',
+      placeholder: 'Password',
+      errorMessage: 'Password should be 8-20 characters and should include at least 1 letter, 1 number and 1 special character!',
+      label: 'Password:',
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true
+    },
+    {
+      id: 5,
+      name: 'confirmPassword',
+      type: 'password',
+      placeholder: 'Confirm Password',
+      errorMessage: 'Password doesnt match!',
+      label: 'Confirm Password:',
+      pattern: values.password,
+      required: true
+    }
+  ]
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(usernameRef)
+    // const data = new FormData(e.target)
+    // console.log(Object.fromEntries(data.entries()))
   }
 
+  const onChange = (e) => {
+      setValues({ ...values, [e.target.name]: e.target.value})
+  }
+
+  console.log(values)
   return (
-    <div className='flex flex-col p-4'>
-      <form onSubmit={handleSubmit}>
-        RegisterForm
-        <FormInput />
-        <FormInput 
+    <div className='flex flex-col p-4 m-4 border rounded bg-grey-light'>
+      <form
+        onSubmit={handleSubmit}
+        className='flex flex-col'
+       >
+        <div className='flex justify-center'>
+          <h1 className='text-lg font-semibold text-pinkyred'>Register Page</h1>
+        </div>
+        {/* <FormInput 
           placeholder="Username"
-          refer={usernameRef}
-        />
-        <FormInput placeholder="Email" />
-        <FormInput placeholder='Full Name'/>
-        <FormInput placeholder='SomeThing Else'/>
-        <button>Submit</button>
+          name="username"
+        /> */}
+        {inputs.map((input) => (
+          <FormInput 
+            key={input.id} {...input} 
+            value={values[input.name]} 
+            onChange={onChange}
+            />
+        ))}
+       
+        {/* <div className='flex justify-center '> */}
+          <button className='p-2 my-6 border rounded bg-slate-400 cursor-pointer'>Submit</button>
+        {/* </div> */}
       </form>
     </div>
   )
